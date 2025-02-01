5b3fd9d713ec2b353cb4646a405b9138f94e7fd0
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-02 01:18:22 +0530

    fix: fixed bug where when moving to different screen after searching for a job and coming back to listing page, the search was getting clear but the filtered products were still intact

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,14 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { updateSearchFilter } from "../features/jobSlice";
+import { useState } from "react";
 
 const Search = () => {
   const dispatch = useDispatch();
+  const [searchValue, setSearchValue]= useState("");
+  const {filter} = useSelector(state=> state.jobs);
 
   const searchHandler = (e) => {
+    setSearchValue(e.target.value)
     dispatch(updateSearchFilter(e.target.value.trim().toLowerCase()));
   };
 
@@ -14,6 +18,7 @@ const Search = () => {
         type="text"
         placeholder="Search by job title..."
         className="form-control"
+        value={searchValue || filter}
         onChange={searchHandler}
       />
     </div>
